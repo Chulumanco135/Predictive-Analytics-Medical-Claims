8a29a4b95a04c14dc486d6729f4c1340be43d97e
Chulumanco Hlalukana <Chulumanco135@gmail.com>
2026-06-17 21:47:40 +0200

    Update feature_engineering.R

--- scripts/feature_engineering.R
+++ scripts/feature_engineering.R
@@ -1,1 +1,7 @@
+Feature Engineering
+
+Feature engineering was performed to create new variables and transform existing variables into formats suitable for predictive modelling.
+New variables such as Processing Delays and the binary target variable Delay Status were created to measure healthcare claim processing delays.
+Processing Delays was calculated using the difference between the invoice date and capture date, while DelayStatus classified claims as delayed (1) or non-delayed (0). 
+In addition, categorical variables were transformed into numerical and binary formats to improve model performance and predictive accuracy.
 
